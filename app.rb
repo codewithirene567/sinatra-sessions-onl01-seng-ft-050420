df4211ab28b355d6af4d637c5a73667d0e7ea826
Irene Scott <irenejinscott@gmail.com>
2020-06-22 14:10:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -61,6 +61,7 @@ session[:id] = 1
 
   get '/logout' do
     #clear session hash here
+    session.clear
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
 
