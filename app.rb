8f47d00652d9dcd5c96a2bcedf87806d1cf2a602
Irene Scott <irenejinscott@gmail.com>
2020-06-22 13:52:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -41,6 +41,7 @@ end
 
   get '/second_exercise' do
     "Your second lesson will be to set your session :id key to a value.\nIn the route: /set_session, write a line of code that sets the session[:id] equal to 1.\nThen, navigate to the '/set_session' path"
+     session[:id] == 1
   end
 
   get '/set_session' do
